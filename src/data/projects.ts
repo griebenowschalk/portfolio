dfd714e33eb500e12e5a1c9f1cddf81fb1fe40be
Schalk Griebenow <schalkgriebenow@Scalk-Griebenow.local>
2025-10-15 10:11:47 +0200

    More projects added

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -11,6 +11,8 @@ export const projectsButtons: string[] = [
   "Unit Tests",
   "MongoDB",
   "Express",
+  "Vite",
+  "Docker",
 ];
 
 export const projectsData: ProjectsData[] = [
@@ -24,10 +26,11 @@ export const projectsData: ProjectsData[] = [
   },
   {
     name: "User Importer",
-    image: "/projects/image-2.jpg",
+    image:
+      "https://api.microlink.io/?url=https%3A%2F%2Fuser-importer.netlify.app%2F&screenshot=true&embed=screenshot.url",
     description: "Cleans user data before importing into a database",
     link: "https://github.com/griebenowschalk/user-importer",
-    tags: ["ReactJS", "TailwindCSS", "Typescript"],
+    tags: ["ReactJS", "TailwindCSS", "Typescript", "Vite"],
   },
   {
     name: "My Todo App",
@@ -35,7 +38,15 @@ export const projectsData: ProjectsData[] = [
       "https://api.microlink.io/?url=https%3A%2F%2Fmy-todo-app-production-6f44.up.railway.app%2F&screenshot=true&embed=screenshot.url",
     description: "Todo app built with Next.js and TailwindCSS",
     link: "https://github.com/griebenowschalk/my-todo-app",
-    tags: ["Next.js", "TailwindCSS", "Node.js", "Unit Tests"],
+    tags: ["Next.js", "TailwindCSS", "Node.js", "Unit Tests", "Vite"],
+  },
+  {
+    name: "Invoice App",
+    image:
+      "https://api.microlink.io/?url=https%3A%2F%2Fbilling-buddy.netlify.app%2F&screenshot=true&embed=screenshot.url",
+    description: "Invoice app built with Next.js and TailwindCSS",
+    link: "https://github.com/griebenowschalk/invoice-app",
+    tags: ["Next.js", "TailwindCSS"],
   },
   {
     name: "Bootcamp API",
@@ -44,5 +55,37 @@ export const projectsData: ProjectsData[] = [
     description: "API for a bootcamp application",
     link: "https://github.com/griebenowschalk/bootcamp-api",
     tags: ["Node.js", "Express", "TypeScript", "MongoDB"],
+  },
+  {
+    name: "Business Feed",
+    image:
+      "https://api.microlink.io/?url=https%3A%2F%2Fbusiness-feed.netlify.app%2F&screenshot=true&embed=screenshot.url",
+    description: "Business feed app built with ReactJS",
+    link: "https://github.com/griebenowschalk/business-feed-project",
+    tags: ["ReactJS", "Unit Tests", "Vite"],
+  },
+  {
+    name: "Awesome FAQ Page",
+    image:
+      "https://api.microlink.io/?url=http%3A%2F%2Fawesome-faq.netlify.app%2F&screenshot=true&embed=screenshot.url",
+    description: "Awesome FAQ page built with ReactJS",
+    link: "https://github.com/griebenowschalk/awesome-faq-project",
+    tags: ["ReactJS", "Unit Tests", "Vite"],
+  },
+  {
+    name: "Testing Basics",
+    image:
+      "https://api.microlink.io/?url=https%3A%2F%2Fgithub.com%2Fgriebenowschalk%2Ftesting-basics&screenshot=true&embed=screenshot.url",
+    description: "Testing basics built with ReactJS and Vitest",
+    link: "https://github.com/griebenowschalk/testing-basics",
+    tags: ["ReactJS", "Unit Tests", "Vite"],
+  },
+  {
+    name: "Docker Basics",
+    image:
+      "https://api.microlink.io/?url=https%3A%2F%2Fgithub.com%2Fgriebenowschalk%2Fdocker-basics&screenshot=true&embed=screenshot.url",
+    description: "Docker basics built with Docker",
+    link: "https://github.com/griebenowschalk/docker-basics",
+    tags: ["Docker"],
   },
 ];
